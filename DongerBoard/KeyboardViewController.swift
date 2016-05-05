19bbcbcc1c2ad1fbc09c03050e69ab443b065836
Kirby Kohlmorgen <kirby@kkohlmorgen.com>
2016-05-05 18:12:15 -0400

    Add basic ༼つ ◕_◕ ༽つ button

--- DongerBoard/KeyboardViewController.swift
+++ DongerBoard/KeyboardViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class KeyboardViewController: UIInputViewController {
 
     var nextKeyboardButton: UIButton!
+    var dongerButton: UIButton!
 
     override func updateViewConstraints() {
         super.updateViewConstraints()
@@ -18,19 +19,7 @@ class KeyboardViewController: UIInputViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        self.nextKeyboardButton = UIButton(type: .System)
-    
-        self.nextKeyboardButton.setTitle("Next Keyboard", forState: .Normal)
-        self.nextKeyboardButton.sizeToFit()
-        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
-    
-        self.nextKeyboardButton.addTarget(self, action: #selector(UIInputViewController.advanceToNextInputMode), forControlEvents: .TouchUpInside)
-        
-        self.view.addSubview(self.nextKeyboardButton)
-    
-        self.nextKeyboardButton.leftAnchor.constraintEqualToAnchor(self.view.leftAnchor).active = true
-        self.nextKeyboardButton.bottomAnchor.constraintEqualToAnchor(self.view.bottomAnchor).active = true
+        self.addButtons()
     }
 
     override func didReceiveMemoryWarning() {
@@ -49,5 +38,46 @@ class KeyboardViewController: UIInputViewController {
         
         self.nextKeyboardButton.setTitleColor(textColor, forState: .Normal)
     }
+    
+    func addButtons() {
+        self.addNextKeyboardButton()
+        self.addDongerButton()
+    }
 
+    func addNextKeyboardButton() {
+        self.nextKeyboardButton = UIButton(type: .System)
+        
+        self.nextKeyboardButton.setTitle("Next Keyboard", forState: .Normal)
+        self.nextKeyboardButton.sizeToFit()
+        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.nextKeyboardButton.addTarget(self, action: #selector(UIInputViewController.advanceToNextInputMode), forControlEvents: .TouchUpInside)
+        
+        self.view.addSubview(self.nextKeyboardButton)
+        
+        // pin to the botton left corner
+        self.nextKeyboardButton.leftAnchor.constraintEqualToAnchor(self.view.leftAnchor).active = true
+        self.nextKeyboardButton.bottomAnchor.constraintEqualToAnchor(self.view.bottomAnchor).active = true
+    }
+    
+    func addDongerButton() {
+        self.dongerButton = UIButton(type: .System)
+        
+        self.dongerButton.setTitle("༼つ ◕_◕ ༽つ", forState: .Normal)
+        self.dongerButton.sizeToFit()
+        self.dongerButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.dongerButton.addTarget(self, action: #selector(self.didTapDongerButton), forControlEvents: .TouchUpInside)
+        
+        self.view.addSubview(self.dongerButton)
+        
+        // center horizontally and vertically
+        self.dongerButton.centerXAnchor.constraintEqualToAnchor(self.view.centerXAnchor).active = true
+        self.dongerButton.centerYAnchor.constraintEqualToAnchor(self.view.centerYAnchor).active = true
+    }
+    
+    func didTapDongerButton() {
+        let proxy = self.textDocumentProxy
+        proxy.insertText("༼つ ◕_◕ ༽つ")
+    }
 }
