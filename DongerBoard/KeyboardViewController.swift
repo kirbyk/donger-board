103451ee7843572c3430577b7f74cf1e75a43157
Kirby Kohlmorgen <kirby@kkohlmorgen.com>
2016-05-05 18:02:05 -0400

    Cleanup iOS keyboardViewController template

--- DongerBoard/KeyboardViewController.swift
+++ DongerBoard/KeyboardViewController.swift
@@ -10,25 +10,22 @@ import UIKit
 
 class KeyboardViewController: UIInputViewController {
 
-    @IBOutlet var nextKeyboardButton: UIButton!
+    var nextKeyboardButton: UIButton!
 
     override func updateViewConstraints() {
         super.updateViewConstraints()
-    
-        // Add custom view sizing constraints here
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        // Perform custom UI setup here
         self.nextKeyboardButton = UIButton(type: .System)
     
-        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), forState: .Normal)
+        self.nextKeyboardButton.setTitle("Next Keyboard", forState: .Normal)
         self.nextKeyboardButton.sizeToFit()
         self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
     
-        self.nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside)
+        self.nextKeyboardButton.addTarget(self, action: #selector(UIInputViewController.advanceToNextInputMode), forControlEvents: .TouchUpInside)
         
         self.view.addSubview(self.nextKeyboardButton)
     
@@ -38,23 +35,18 @@ class KeyboardViewController: UIInputViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated
-    }
-
-    override func textWillChange(textInput: UITextInput?) {
-        // The app is about to change the document's contents. Perform any preparation here.
     }
 
     override func textDidChange(textInput: UITextInput?) {
-        // The app has just changed the document's contents, the document context has been updated.
-    
         var textColor: UIColor
         let proxy = self.textDocumentProxy
+        
         if proxy.keyboardAppearance == UIKeyboardAppearance.Dark {
             textColor = UIColor.whiteColor()
         } else {
             textColor = UIColor.blackColor()
         }
+        
         self.nextKeyboardButton.setTitleColor(textColor, forState: .Normal)
     }
 
