f167902f513e6ede71a90a9ec98a84b6e2c1487c
Kirby Kohlmorgen <kirby@kkohlmorgen.com>
2016-05-05 16:58:26 -0400

    Initial Commit

---
+++ Donger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Donger
+//
+//  Created by Kirby Kohlmorgen on 5/5/16.
+//  Copyright © 2016 Kirby Kohlmorgen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DongerTests/DongerTests.swift
@@ -0,0 +1,36 @@
+//
+//  DongerTests.swift
+//  DongerTests
+//
+//  Created by Kirby Kohlmorgen on 5/5/16.
+//  Copyright © 2016 Kirby Kohlmorgen. All rights reserved.
+//
+
+import XCTest
+@testable import Donger
+
+class DongerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
