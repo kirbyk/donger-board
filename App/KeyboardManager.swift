ea2e72aed0745591b4398a6b10542c081da64610
Everett Berry <epberry@purdue.edu>
2016-05-26 02:59:56 -0400

    files needed for RN to talk to Swift

---
+++ App/KeyboardManager.swift
@@ -0,0 +1,9 @@
+//
+//  KeyboardManager.swift
+//  DongerBoard
+//
+//  Created by Everett Berry on 5/25/16.
+//  Copyright © 2016 Kirby Kohlmorgen. All rights reserved.
+//
+
+import Foundation
---
+++ App/KeyboardManagerBridge.m
@@ -0,0 +1,9 @@
+//
+//  RNBridgeManager.m
+//  DongerBoard
+//
+//  Created by Everett Berry on 5/25/16.
+//  Copyright © 2016 Kirby Kohlmorgen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
