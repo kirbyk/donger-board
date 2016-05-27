33502887667c5fe31f20ede889d2f7022a787a6f
Everett Berry <epberry@purdue.edu>
2016-05-27 00:38:02 -0400

    changing keyboard color with sliders in keyboard app

--- index.ios.js
+++ index.ios.js
@@ -7,7 +7,9 @@ import {
   Text,
   View,
   Picker,
-  NativeModules
+  NativeModules,
+  Slider,
+  TouchableOpacity
 } from 'react-native';
 
 var styles = StyleSheet.create({
@@ -27,18 +29,25 @@ class DongerBoardApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      color: "red"
+      color: "red",
+      redValue: 0,
+      greenValue: 0,
+      blueValue: 0
     };
-    /* TODO: use later for Picker
-          selectedValue={this.state.color}
-          onValueChange={(lang) => this.setState({color: "Blue"})}>
-          */
   }
 
   pickerValueChanged(color) {
     console.log("JS: picker value changed to " + color);
-    NativeModules.KeyboardManager.recordColor(color);
+    // NativeModules.KeyboardManager.recordColor(color);
     this.setState({color: color});
+  }
+
+  onPressButton() {
+    console.log("change keyboard color");
+    NativeModules.KeyboardManager.recordColor(this.state.color, 
+        this.state.redValue, 
+        this.state.greenValue, 
+        this.state.blueValue);
   }
 
   render() {
@@ -50,12 +59,43 @@ class DongerBoardApp extends React.Component {
         </Text>
       </View>
       <View>
+        <Text>Choose a preset color</Text>
         <Picker 
           selectedValue={this.state.color}
           onValueChange={(val) => this.pickerValueChanged(val)}>
           <Picker.Item label="Red" value="red" />
           <Picker.Item label="Blue" value="blue" />
         </Picker>
+
+        <Text>Red</Text>
+        <Text style={styles.text} >
+          {(this.state.redValue * 256).toFixed(0)}
+        </Text>
+        <Slider
+          {...this.props}
+          onValueChange={(value) => this.setState({redValue: value})} 
+        />
+
+        <Text>Green</Text>
+        <Text style={styles.text} >
+          {(this.state.greenValue * 256).toFixed(0)}
+        </Text>
+        <Slider
+          {...this.props}
+          onValueChange={(value) => this.setState({greenValue: value})} 
+        />
+
+        <Text>Blue</Text>
+        <Text style={styles.text} >
+          {(this.state.blueValue * 256).toFixed(0)}
+        </Text>
+        <Slider
+          {...this.props}
+          onValueChange={(value) => this.setState({blueValue: value})} 
+        />
+        <TouchableOpacity onPress={this.onPressButton.bind(this)}>
+          <Text>Set Keyboard Color</Text>
+        </TouchableOpacity>
       </View>
       </View>
     )
