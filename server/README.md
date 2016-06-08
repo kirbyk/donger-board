8515d0296c12e403d95c0767a81aeb0b747bf3eb
Kirby Kohlmorgen <kirby@kkohlmorgen.com>
2016-06-07 19:14:26 -0700

    Add endpoint docs. Add gitignore. Remove bin. Add basic json service

--- server/README.md
+++ server/README.md
@@ -17,3 +17,30 @@ make install
 export GOBIN=`pwd`/bin
 make
 ```
+
+## Endpoints
+---------
+
+#### /v1/trending/poll
+
+**Example Request:**
+
+```
+POST /v1/trending/poll HTTP/1.1
+Content-Type: application/json
+
+{
+  "userId": "1234567890ABCDEF",
+  "ts": 1465351791910,
+  "dongerId" "938"
+}
+```
+
+**Example Response:**
+
+```json
+{
+  "status": 200,
+  "message": "Successfully submitted trending poll."
+}
+```
--- server/main.go
+++ server/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
+
+type Book struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
 
 func main() {
 	r := mux.NewRouter()
@@ -34,9 +40,20 @@ func ProductHandler(w http.ResponseWriter, req *http.Request) {
 	log.Println("serving", req.URL)
 	log.Println(req.UserAgent())
 
-	vars := mux.Vars(req)
-	name := vars["key"]
+	// vars := mux.Vars(req)
+	// name := vars["key"]
 
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintln(w, "Welcome to /products/", name)
+	// w.WriteHeader(http.StatusOK)
+	// fmt.Fprintln(w, "Welcome to /products/", name)
+
+	book := Book{"Building Web Apps with Go", "Jeremy Saenz"}
+
+	js, err := json.Marshal(book)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
